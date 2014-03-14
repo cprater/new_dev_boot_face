05da025f7f4c9fb5bb86670557de44130295ba5f
Chris Prater <cprater23@gmail.com>
2014-03-14 18:21:56 -0500

    .gitignore is working

--- app/models/group.rb
+++
@@ -1,9 +0,0 @@
-class Group < ActiveRecord::Base
-
-	belongs_to :user
-  has_many :groups_users
-  has_many :users, through: :groups_users
-  belongs_to :creator, class_name: :user, foreign_key: :created_by
-  has_many :posts
-
-end
--- app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ActiveRecord::Base
-
-  has_many :comments, as: :commentable
-  belongs_to :user
-  belongs_to :group
-
-end
-
