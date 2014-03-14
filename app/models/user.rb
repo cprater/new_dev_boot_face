a6ce9edeecb2da7c2ca2140581ed38730af27c89
Chris Prater <cprater23@gmail.com>
2014-03-14 18:05:30 -0500

    group creator can delete group

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
+  
   has_many :comments
-  has_many :posts
-  has_and_belongs_to_many :groups
+  
+  has_many :groups_users
+  has_many :groups, through: :groups_users
+
+  has_many :created_groups, class_name: :group, foreign_key: :created_by
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def full_name
-    self.first_name + " " + self.last_name
-  end
+
 end
